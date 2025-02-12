b31bc483abe60ebdaf578094e1600fc3b1bbc7d5
Brayan <brayan.orecon@gmail.com>
2025-02-12 15:46:05 -0500

    Añadiendo ApiFetcher para consumir API

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import ApiFetcher from "./components/ApiFetcher";
+
+function App() {
+  return (
+    <div className="p-4">
+    <h1 className="text-xl font-bold">Mi Aplicación IDP</h1>
+    <ApiFetcher />
+  </div>
+);
+}
+
+export default App;
